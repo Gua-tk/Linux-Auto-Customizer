ed3ee3936e965bd80a3f367d9eb1f94a10861b78
AleixMT <aleix.marine@estudiants.urv.cat>
2019-10-22 14:20:37 +0200

    adddded latex

--- set_environment.sh
+++ set_environment.sh
@@ -148,6 +148,10 @@ if [ "$(whoami)" != "root" ]; then
 		echo "#!/usr/bin/env bash" > New_Shell_Script.sh
 		echo "#!/usr/bin/env python3" > New_Python3_Script.py
 		echo "#!/usr/bin/env python2" > New_Python2_Script.py
+		echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%2345678901234567890123456789012345678901234567890123456789012345678901234567890
+%        1         2         3         4         5         6         7         8
+" > New_LaTeX_Document.tex
 		chmod 755 *
 		cd ..
 	fi
@@ -233,6 +237,8 @@ Exec=sublime"
 		echo -e "$sublime_launcher" > ~/.local/share/applications/sublime.desktop
 		chmod 775 ~/.local/share/applications/sublime.desktop
 	fi
+
+
 else
 	##### Software #####
 	# Update repositories and system
@@ -254,6 +260,9 @@ else
         apt install -y -qq ./google-chrome*.deb
         rm google-chrome*.deb
     fi
+
+    # LaTeX
+    apt-get -y -qq install texlive-latex-extra
 
 	# Clean
 	apt -y -qq autoremove
