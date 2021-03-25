881878529db08329777a7585f0be6fc40fd52534
Axlfc <axelfernandezcurros@gmail.com>
2021-03-25 05:14:35 +0100

    changes

--- common_data.sh
+++ common_data.sh
@@ -567,29 +567,89 @@ youtubewav_alias="alias youtubewav=\"youtube-dl --extract-audio --audio-format w
 ### SYSTEM FEATURE RELATED VARIABLES ###
 
 converters_downloader="https://github.com/Axlfc/converters"
-converters_functions="bintohex()
+converters_functions="bintooct()
 {
-  bintodec \$1 | dectohex
+  to \$1 2 3
 }
-bintoutf()
+bintoocto()
 {
-  bintodec \$1 | dectoutf
+  to \$1 2 8
 }
+bintodec()
+{
+  to \$1 2 10
+}
+bintohex()
+{
+  to \$1 2 16
+}
+
+octtobin()
+{
+  to \$1 3 2
+}
+octtoocto()
+{
+  to \$1 3 8
+}
+octtodec()
+{
+  to \$1 3 10
+}
+octohex()
+{
+  to \$1 3 16
+}
+
+octotobin()
+{
+  to \$1 8 2
+}
+octotooct()
+{
+  to \$1 8 3
+}
+octotodec()
+{
+  to \$1 8 10
+}
+octotohex()
+{
+  to \$1 8 16
+}
+
+dectobin()
+{
+  to \$1 10 2
+}
+dectooct()
+{
+  to \$1 10 3
+}
+dectoocto()
+{
+  to \$1 10 8
+}
+dectohex()
+{
+  ro \$1 10 16
+}
+
 hextobin()
 {
-  hextodec \$1 | dectobin
+  to \$1 16 2
 }
-hextoutf()
+hextooct()
 {
-  hextodec \$1 | dectoutf
+  to \$1 16 3
 }
-utftobin()
+hextoocto()
 {
-  utftodec \$1 | dectobin
+  to \$1 16 8
 }
-utftohex()
+hextodec()
 {
-  utftodec \$1 | dectohex
+  to \$1 16 10
 }"
 
 extract_function="
