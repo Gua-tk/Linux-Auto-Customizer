069f43376800ffd829b2fcbda002d6a994d16bc0
AleixMT <aleixaretra@gmail.com>
2021-05-06 13:17:11 +0200

    safe copy adding to prompt

--- common_data.sh
+++ common_data.sh
@@ -476,7 +476,73 @@ StartupNotify=true"
 
 openoffice_downloader="https://downloads.sourceforge.net/project/openofficeorg.mirror/4.1.9/binaries/en-US/Apache_OpenOffice_4.1.9_Linux_x86-64_install-deb_en-US.tar.gz?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fopenofficeorg.mirror%2Ffiles%2F4.1.9%2Fbinaries%2Fen-US%2FApache_OpenOffice_4.1.9_Linux_x86-64_install-deb_en-US.tar.gz%2Fdownload&ts=1614201028"
 
-prompt_function="export PS1=\"\\[\\\e[1;37m\\]\\\\\\d \\\\\\\t \\[\\\e[0;32m\\]\\\\\u\[\\\e[4;35m\\]@\\[\\\e[0;36m\\]\\\\\\H\\[\\\e[0;33m\\] \\\\\\w\\[\\\e[0;32m\\] \\\\\\\$ \""
+prompt_function="
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z \"\${debian_chroot:-}\" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=\$(cat /etc/debian_chroot)
+fi
+
+# set a fancy prompt (non-color, unless we know we \"want\" color)
+case \"\$TERM\" in
+    xterm-color|*-256color) color_prompt=yes;;
+esac
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+#force_color_prompt=yes
+
+if [ -n \"\$force_color_prompt\" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	# We have color support; assume it's compliant with Ecma-48
+	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+	# a case would tend to support setf rather than setaf.)
+	color_prompt=yes
+    else
+	color_prompt=
+    fi
+fi
+
+if [ \"\$color_prompt\" = yes ]; then
+    PS1='\${debian_chroot:+(\$debian_chroot)}\[\\\033[01;32m\\]\u@\h\[\\\033[00m\]:\[\\\033[01;34m\]\w\[\\\033[00m\]\\$ '
+else
+    PS1='\${debian_chroot:+(\$debian_chroot)}\u@\h:\w\\$ '
+fi
+unset color_prompt force_color_prompt
+
+# If this is an xterm set the title to user@host:dir
+case \"\$TERM\" in
+xterm*|rxvt*)
+    PS1=\"\\[\\\e]0;\${debian_chroot:+(\$debian_chroot)}\u@\h: \w\\\a\]\$PS1\"
+    ;;
+*)
+    ;;
+esac
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval \"\$(dircolors -b ~/.dircolors)\" || eval \"\$(dircolors -b)\"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# colored GCC warnings and errors
+#export GCC_COLORS=\"error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01\"
+
+
+
+# Colorful PS1
+#export PS1=\"\$PS1\\[\\\e[1;37m\\]\\\\\\d \\\\\\\t \\[\\\e[0;32m\\]\\\\\u\[\\\e[4;35m\\]@\\[\\\e[0;36m\\]\\\\\\H\\[\\\e[0;33m\\] \\\\\\w\\[\\\e[0;32m\\] \\\\\\\$ \"
+
+
+
+
+"
 
 pycharm_downloader=https://download.jetbrains.com/python/pycharm-professional-2020.3.5.tar.gz
 pycharm_launcher="[Desktop Entry]
@@ -527,7 +593,7 @@ HISTIGNORE=\"ls:ps:history:l:pwd:top:gitk\"
 # same history entry, adding semicolons where necessary to preserve syntactic correctness.
 shopt -s cmdhist
 # Save and reload from history before prompt appears
-export PROMPT_COMMAND=\"history -a; history -r\"
+export PROMPT_COMMAND=\"history -a; history -r; ${PROMPT_COMMAND}\"
 "
 
 shortcut_aliases="export DESK=${XDG_DESKTOP_DIR}
