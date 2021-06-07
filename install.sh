ea92c1ea6447e0e0d98c20e8c33f013258035200
Axlfc <axelfernandezcurros@gmail.com>
2021-06-07 14:19:53 +0200

    small changes to caffeine autostart

--- install.sh
+++ install.sh
@@ -77,7 +77,7 @@ install_caffeine()
   # patch to start caffeine-indicator activated
   wget -O - https://gist.githubusercontent.com/syneart/aa8f2f27a103a7f1e1812329fa192e65/raw/caffeine-indicator.patch | sudo patch /usr/bin/caffeine-indicator
   # copy caffeine-indicator launcher to autostart directory
-  cp -p "/usr/share/applications/caffeine-indicator.desktop" "${HOME_FOLDER}/.config/autostart/caffeine-indicator.desktop"
+  cp -p "${ALL_USERS_LAUNCHERS_DIR}/caffeine-indicator.desktop" "${HOME_FOLDER}/.config/autostart/caffeine-indicator.desktop"
 }
 
 install_calibre()
