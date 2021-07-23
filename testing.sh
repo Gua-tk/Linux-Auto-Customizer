11076c96f071d74bd1779ee089629f18e0160ec0
AleixMT <aleix.marine@estudiants.urv.cat>
2021-07-23 04:33:03 +0200

    refactored testing to wait for pid

--- testing.sh
+++ testing.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Install all programs, show output and save it to a file, ignore errors, overwrite already existent features
-gnome-terminal -- sudo bash install.sh -v -o -i --root 2>&1 | tee customizersudoall.outerr &
+sudo bash install.sh -v -o -i --root 2>&1 | tee customizersudoall.outerr &
 PID_ROOT=$!
-gnome-terminal -- bash install.sh -v -o -i --user 2>&1 | tee customizeruserall.outerr &
+bash install.sh -v -o -i --user 2>&1 | tee customizeruserall.outerr &
 PID_USER=$!
 wait $PID_ROOT
 wait $PID_USER
