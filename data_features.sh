0dc24859f5b45ef69bb8c9c650a1fa8743076259
AleixMT <aleix.marine@estudiants.urv.cat>
2021-07-23 13:53:06 +0200

    solved more problmes of testing now with the history optimization

--- data_features.sh
+++ data_features.sh
@@ -1073,8 +1073,13 @@ HISTTIMEFORMAT='%F %T '
 shopt -s checkwinsize  # Kinda buggy
 
 # Save and reload from history before prompt appears
-if [[ -z \"\$(echo \${PROMPT_COMMAND} | grep -Fo \"history -a; history -r; \")\" ]]; then
-  export PROMPT_COMMAND=\"\${PROMPT_COMMAND}; history -a; history -r\"
+if [ -z \"\$(echo \"\${PROMPT_COMMAND}\" | grep -Fo \"history -a; history -r\")\" ]; then
+  # Check if there is something inside PROMPT_COMMAND, so we put semicolon to separate or not
+  if [ -z \"\${PROMPT_COMMAND}\" ]; then
+    export PROMPT_COMMAND=\"history -a; history -r\"
+  else
+    export PROMPT_COMMAND=\"\${PROMPT_COMMAND}; history -a; history -r\"
+  fi
 fi
 ")
 
