03a1d12b3497a5f3227364c5105054ee10d85622
AleixMT <aleix.marine@estudiants.urv.cat>
2021-07-23 17:28:02 +0200

    changed testing to be more visualize

--- testing.sh
+++ testing.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 # Install all programs, show output and save it to a file, ignore errors, overwrite already existent features
-sudo bash install.sh -v -o -i --root 2>&1 | tee customizersudoall.outerr.txt &
+sudo bash install.sh -v -o -i --root &>customizersudoall.outerr.txt &
 PID_ROOT=$!
-# wait 10 seconds to write the password
+# wait 10 seconds to write the password with nothing on the screen
 sleep 10
-bash install.sh -v -o -i --user 2>&1 | tee customizeruserall.outerr.txt &
+bash install.sh -v -o -i --user &>customizeruserall.outerr.txt &
 PID_USER=$!
+
+gnome-terminal -- less +F -f -r customizeruserall.outerr.txt
+gnome-terminal -- less +F -f -r customizersudoall.outerr.txt
 wait $PID_ROOT
 wait $PID_USER
 if [ "$1" == "shutdown" ]; then
