2e98c3c744c66aaba0729f9de08f61a68a8034f9
AleixMT <aleix.marine@estudiants.urv.cat>
2021-07-23 04:36:30 +0200

    changed minor thing in testing

--- testing.sh
+++ testing.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Install all programs, show output and save it to a file, ignore errors, overwrite already existent features
-sudo bash install.sh -v -o -i --root 2>&1 | tee customizersudoall.outerr &
+sudo bash install.sh -v -o -i --root 2>&1 | tee customizersudoall.outerr.txt &
 PID_ROOT=$!
-bash install.sh -v -o -i --user 2>&1 | tee customizeruserall.outerr &
+bash install.sh -v -o -i --user 2>&1 | tee customizeruserall.outerr.txt &
 PID_USER=$!
 wait $PID_ROOT
 wait $PID_USER
