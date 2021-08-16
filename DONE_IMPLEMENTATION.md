8dca2f8660f87b5fb7659b81332d3e37949276cd
Axlfc <axelfernandezcurros@gmail.com>
2021-08-16 22:19:21 +0200

    edit

--- DONE_IMPLEMENTATION.md
+++ DONE_IMPLEMENTATION.md
@@ -112,6 +112,7 @@
 - [x] `data_features.sh`: Update history [optimization](https://unix.stackexchange.com/questions/6628/what-customizations-have-you-done-on-your-shell-profile-to-increase-productivity)
 - [x] `data_features.sh`: `L` function columns, also the alias alias totalusage="df -hl --total | grep total" can be rewritted as an alias or case `L /` of L because it uses du
 - [x] `clone`: either access url without writing https://
+- [x] `pluma`, `edit` functions should be able to open multiple files at once
 
 
 ###### Axel
--- data_features.sh
+++ data_features.sh
@@ -876,21 +876,24 @@ edit() {
     [sshconf]=\"${HOME_FOLDER}/.ssh/config\"
     [tmuxconf]=\"${HOME_FOLDER}/.tmux.conf\"
     )
-  if [ -n \"\$1\" ]; then
-    local path_editable=\"\${EDITABLEFILES[\"\$1\"]}\"
-    if [ -z \"\${path_editable}\" ]; then
-      if [ -f \"\$1\" ]; then
-        nohup editor \"\$1\" &>/dev/null &
-      else
-        echo \"\$1 is not a valid file or option.\"
-      fi
-    else
-      nohup editor \"\${path_editable}\" &>/dev/null &
-    fi
-  else  # If not an argument show the dictionary structure
+  if [ \$# -eq 0 ]; then
     echo \"Recognised arguments to edit:\"
     for i in \"\${!EDITABLEFILES[@]}\"; do
       echo \"\${i}:\${EDITABLEFILES[\${i}]}\"
+    done
+  else
+    while [ -n \"\$1\" ]; do
+      local path_editable=\"\${EDITABLEFILES[\"\$1\"]}\"
+      if [ -z \"\${path_editable}\" ]; then
+        if [ -f \"\$1\" ]; then
+          nohup editor \"\$1\" &>/dev/null &
+        else
+          echo \"\$1 is not a valid file or option.\"
+        fi
+      else
+        nohup editor \"\${path_editable}\" &>/dev/null &
+      fi
+      shift
     done
   fi
 }
