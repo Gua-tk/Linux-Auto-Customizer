7dc86a486b5a4a58cbdddd39fac3a122bdecd966
Axlfc <axelfernandezcurros@gmail.com>
2021-08-30 00:25:02 +0200

    refined loc function

--- data_features.sh
+++ data_features.sh
@@ -3940,37 +3940,59 @@ loc_installationtype="environmental"
 loc_arguments=("loc" "function_loc")
 loc_bashfunctions=("
 loc() {
+  local total_lines=0
+  local -r NEW_LINE=\$'\\n'
+  local loc_table=\"\"
+
   if [ -z \"\$1\" ]; then
-    echo \"\$(find \"\$1\" -type f -name \"*\" | xargs cat | sed '/^\$/d' | wc -l) lines in code.\"
+    local total_lines_in_directory=0
+    for file_route in \$(find \"\$1\" -type f -name \"*\" 2>/dev/null); do
+      loc_table+=\"\${file_route};\"
+      local lines_file=\"\$(cat \"\${file_route}\" | sed '/^\$/d' | wc -l 2>/dev/null)\"
+      total_lines_in_directory=\$(( \${total_lines_in_directory} + \${lines_file} ))
+      loc_table+=\"\${lines_file}\"
+      loc_table+=\"\${NEW_LINE}\"
+    done
+    loc_table+=\"\${1};\"
+    loc_table+=\"\${total_lines_in_directory}\"
+    total_lines=\$(( \${total_lines} + \${total_lines_in_directory} ))
+    loc_table+=\"\${NEW_LINE}\"
   else
-    local -r NEW_LINE=\$'\\n'
-    local total_lines=0
-    local loc_table=\"\"
+    local counter_args=1
     while [ -n \"\$1\" ]; do
       if [ -f \"\$1\" ]; then
         loc_table+=\"\${1};\"
-        loc_table+=\"\$(cat \"\$1\" | sed '/^\$/d' | wc -l)\"
+        local_lines_in_file=\"\$(cat \"\$1\" | sed '/^\$/d' | wc -l 2>/dev/null)\"
+        loc_table+=\"\${local_lines_in_file}\"
+        total_lines=\$(( \${total_lines} + \${local_lines_in_file} ))
         loc_table+=\"\${NEW_LINE}\"     
       elif [ -d \"\$1\" ]; then
         local total_lines_in_directory=0
 
-        for file_route in \$(find \"\$1\" -type f -name \"*\"); do
+        for file_route in \$(find \"\$1\" -type f -name \"*\" 2>/dev/null); do
           loc_table+=\"\${file_route};\"
-          local lines_file=\"\$(cat \"\${file_route}\" | sed '/^\$/d' | wc -l)\"
+          local lines_file=\"\$(cat \"\${file_route}\" | sed '/^\$/d' | wc -l 2>/dev/null)\"
           total_lines_in_directory=\$(( \${total_lines_in_directory} + \${lines_file} ))
           loc_table+=\"\${lines_file}\"
           loc_table+=\"\${NEW_LINE}\"
         done
         loc_table+=\"\${1};\"
         loc_table+=\"\${total_lines_in_directory}\"
+        total_lines=\$(( \${total_lines} + \${total_lines_in_directory} ))
         loc_table+=\"\${NEW_LINE}\"
       else
-        loc_table+=\"argument;\"
-        loc_table+=\"\$(echo \"\$1\" | sed '/^\$/d' | wc -l)\"
+        loc_table+=\"argument \${counter_args};\"
+        local_lines_in_string=\"\$(echo \"\$1\" | sed '/^\$/d' | wc -l)\"
+        loc_table+=\"\${local_lines_in_string}\"
+        total_lines=\$(( \${total_lines} + \${local_lines_in_string} ))
         loc_table+=\"\${NEW_LINE}\"
       fi
+      counter_args=\$(( 1 + \${counter_args} ))
       shift
     done
+    loc_table+=\"\${NEW_LINE}\"
+    loc_table+=\"TOTAL;\"
+    loc_table+=\"\${total_lines}\"
     echo \"\${loc_table}\" | column -ts \";\"
   fi
 }
