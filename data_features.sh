12f93a8b1ca8786d3fbb14b60b8570ccaa70c373
Axlfc <axelfernandezcurros@gmail.com>
2021-09-02 11:06:49 +0200

    terminal background palette was wrongly placed in bashcolors

--- data_features.sh
+++ data_features.sh
@@ -259,10 +259,6 @@ b_readmeline="| Function \`b\` | Alias for \`bash\` | Alias \`b\` || <ul><li>- [
 
 bashcolors_installationtype="environmental"
 bashcolors_arguments=("bash_colors")
-bashcolors_bashinitializations=("
-##############################################################################################################################grey#######pink#######green######yellow#####purple#####red#######cyan#######orange####lighgrey##lightpink##lightgreen#lightyellow#lightpurple#lightred#lightcyan##lightorange
-gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:\${gnome_terminal_profile}/ palette \"['#262626', '#E356A7', '#42E66C', '#E4F34A', '#9B6BDF', '#E64747', '#75D7EC', '#EFA554', '#7A7A7A', '#FF79C6', '#50FA7B', '#F1FA8C', '#BD93F9', '#FF5555', '#8BE9FD', '#FFB86C']\"
-")
 bashcolors_bashfunctions=("
 colors() {
   if [ -z \"\$(echo \"\${COLORS[@]}\")\" ]; then
@@ -5731,9 +5727,12 @@ if [ -n \"\${profile_uuid}\" ]; then
   gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:\${profile_uuid}/ background-color \"#282A36\"
   gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:\${profile_uuid}/ foreground-color \"#F8F8F2\"
 
+  ##############################################################################################################################grey#######pink#######green######yellow#####purple#####red#######cyan#######orange####lighgrey##lightpink##lightgreen#lightyellow#lightpurple#lightred#lightcyan##lightorange
+  gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:\${profile_uuid}/ palette \"['#262626', '#E356A7', '#42E66C', '#E4F34A', '#9B6BDF', '#E64747', '#75D7EC', '#EFA554', '#7A7A7A', '#FF79C6', '#50FA7B', '#F1FA8C', '#BD93F9', '#FF5555', '#8BE9FD', '#FFB86C']\"
+
   # Cursor like in a text editor
   gsettings set org.gnome.Terminal.Legacy.Profile:/:\"\${profile_uuid}\"/ cursor-shape 'ibeam'
-
+  
   unset profile_uuid
 else
   echo \"ERROR, non terminal default profile list found\"
