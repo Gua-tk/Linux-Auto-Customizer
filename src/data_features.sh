8c05f2ae70ad681c41af9a450836f7a6861a32b1
AleixMT <aleixaretra@gmail.com>
2021-12-10 21:30:14 +0100

    ADDED: gsettings configuration of tweaks in nemo installation

--- src/data_features.sh
+++ src/data_features.sh
@@ -4619,6 +4619,82 @@ fi
 xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
 gsettings set org.gnome.desktop.background show-desktop-icons false
 gsettings set org.nemo.desktop show-desktop-icons true
+
+# Other tweaks
+gsettings set org.gnome.desktop.screensaver ubuntu-lock-on-suspend false
+gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true
+gsettings set org.gnome.desktop.datetime automatic-timezone true
+gsettings set org.gnome.desktop.interface gtk-theme Yaru-dark
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.desktop.interface clock-show-seconds true
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+gsettings set org.gnome.desktop.interface enable-hot-corners true
+gsettings set org.gnome.desktop.interface cursor-theme Yaru-dark
+gsettings set org.gnome.terminal.legacy mnemonics-enabled true
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position \"'BOTTOM'\"
+gsettings set org.gnome.login-screen fallback-logo \"'/usr/share/plymouth/ubuntu-logo.png'\"
+# gsettings set org.gnome.login-screen fallback-logo \"'CUSTOMIZER LOGO'\"
+gsettings set org.gnome.gedit.preferences.editor auto-save true
+gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
+gsettings set org.gnome.gedit.preferences.editor display-right-margin true
+gsettings set org.gnome.gedit.preferences.editor tabs-size 4
+gsettings set org.gnome.gedit.preferences.ui bottom-panel-visible true
+gsettings set org.gnome.gedit.preferences.ui side-panel-visible true
+gsettings set org.gnome.gedit.plugins.spell highlight-misspeled true
+gsettings set org.nemo.desktop home-icon-visible true
+gsettings set org.nemo.icon-view \"['size', 'type', 'date_accessed', 'date_modified']\"
+gsettings set org.gnome.desktop.privacy hide-identity true
+gsettings set org.gnome.calculator show-thousands true
+gsettings set org.nemo.sidebar-panels.tree show-only-directories false
+
+# Category launcher containers in dashboard
+gsettings set org.gnome.desktop.app-folders folder-children \"['accessories', 'chrome-apps', 'games', 'graphics', 'internet', 'office', 'programming', 'science', 'sound---video', 'system-tools', 'universal-access', 'wine']\"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/accessories/ name \"Accessories\"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/accessories/ categories \"['Utility']\"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/chrome-apps/ name \"Chrome Apps\"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/chrome-apps/ categories \"['chrome-apps']\"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/games/ name \"Games\"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/games/ categories \"['Game']\"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/graphics/ name \"Graphics\"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/graphics/ categories \"['Graphics']\"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/internet/ name \"Internet\"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/internet/ categories \"['Network', 'WebBrowser', 'Email']\"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/office/ name \"Office\"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/office/ categories \"['Office']\"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/programming/ name \"Programming\"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/programming/ categories \"['Development']\"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/science/ name \"Science\"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/science/ categories \"['Science']\"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/sound---video/ name \"Sound & Video\"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/sound---video/ categories \"['AudioVideo', 'Audio', 'Video']\"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/system-tools/ name \"System Tools\"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/system-tools/ categories \"['System', 'Settings']\"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/universal-access/ name \"Universal Access\"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/universal-access/ categories \"['Accessibility']\"
+
+
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/wine/ name \"Wine\"
+gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/wine/ categories \"['Wine', 'X-Wine', 'Wine-Programs-Accessories']\"
 "
 
 "
