c833e35bc6ff83f7ae6ca24a1858b46cd6406a5e
AleixMT <aleixaretra@gmail.com>
2021-12-11 21:18:20 +0100

    UPDATED: Externalized files for tmux

---
+++ src/features/tmux_functions.sh
@@ -0,0 +1,165 @@
+
+att()
+{
+	tmux attach -t "$1"
+}
+
+dt()
+{
+	local CONTADOR=0
+	while [ $# -ne 0 ]; do
+		if [ "$1" != "default" ]; then
+		  CONTADOR=$((${CONTADOR} + 1))
+		  tmux new-session -d -s "$1"
+	  fi
+	  shift
+	done
+}
+
+
+freezesession()
+{
+	tmuxp freeze "$1"
+}
+
+
+session()
+{
+	if [ -n "$1" ]; then
+		tmuxp load "€{CURRENT_INSTALLATION_FOLDER}/$1.yaml"
+	else
+		tmuxp load "€{CURRENT_INSTALLATION_FOLDER}/main.yaml"
+	fi
+}
+
+
+ta()
+{
+  if [[ -z $1 ]]; then
+    tmux attach
+  else
+    tmux attach -t "$1"
+  fi
+}
+_ta() {
+  TMUX_SESSIONS=$(tmux ls -F '#S' | xargs)
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $(compgen -W "$TMUX_SESSIONS" -- $cur) )
+}
+complete -F _ta ta
+
+
+td()
+{
+  tmux detach "$1"
+}
+_td() {
+  TMUX_SESSIONS=$(tmux ls -F '#S' | xargs)
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $(compgen -W "$TMUX_SESSIONS" -- $cur) )
+}
+complete -F _td td
+
+
+trs()
+{
+	tmux rename-session "$1"
+}
+
+
+ts()
+{
+	tmux switch -t "$1"
+}
+_ts() {
+  TMUX_SESSIONS=$(tmux ls -F '#S' | xargs)
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  COMPREPLY=( $(compgen -W "$TMUX_SESSIONS" -- $cur) )
+}
+complete -F _ts ts
+
+
+tsend()
+{
+  tmux send -t "$1" "$2" Enter
+}
+
+
+tses()
+{
+	tmuxp load "€{CURRENT_INSTALLATION_FOLDER}/$1.json"
+}
+
+
+tsesconvert()
+{
+	tmuxp convert -y "$1"
+}
+
+
+tsk()
+{
+CONTADOR=0
+while [ "$*" ]
+do
+	if [[ "$1" == "default" ]]; then
+	  shift
+	else
+	  let CONTADOR=$CONTADOR+1
+	  tmux kill-session -t $1
+    shift
+  fi
+done
+}
+_tsk() {
+    TMUX_SESSIONS=$(tmux ls -F '#S' | xargs)
+
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    COMPREPLY=( $(compgen -W "$TMUX_SESSIONS" -- $cur) )
+}
+complete -F _tsk tsk
+
+
+
+alias at="tmux a -t"
+alias edittmux="pluma ${HOME}/.tmux.conf"
+alias oldt="tmux new -s default"
+alias t="tmux"
+alias tl="tmux ls"
+alias tls="tmux ls -F '#{session_attached} #{session_name}'"
+alias tk="tmux kill-ses"
+alias tks="tmux kill-server"
+alias tbash="tmux new-session /bin/bash \; set default-shell /bin/bash"
+alias tds="tmux a #"
+alias terminal="tmux"
+alias tn="tmux new -s $1"
+alias trefresh="tmux refresh-client -S"
+alias tupgrade="tmux source ${HOME}/.tmux.conf"
+alias pbcopy="xsel --clipboard --input"
+alias pbpaste="xsel --clipboard --output"
+
+# create a global per-pane variable that holds the pane's PWD
+export PS9=$PS9'$( [ -n $TMUX ] && tmux setenv -g TMUX_PWD_$(tmux display -p "#D" | tr -d %) $PWD)'
+
+echo "tmux features"
+
+# Load tmux in terminal
+if [ -z "$TMUX" ]; then
+    echo "hola, sessio atachada"
+    sleep 2
+    attach_session=$(tmux 2> /dev/null ls -F '#{session_attached} #{?#{==:#{session_last_attached},},1,#{session_last_attached}} #{session_id}' | awk '/^0/ { if ($2 > t) { t = $2; s = $3 } }; END { if (s) printf "%s", s }')
+    echo "I finished attach session"
+    sleep 2
+    if [ -n "$attach_session" ]; then
+      echo "Hem trobat la sessió"
+      sleep 2
+      tmux attach -t "$attach_session"
+    else
+      echo "NO TROBO RE"
+      sleep 2
+      if [ ${EUID} -ne 0 ]; then
+        tmux
+      fi
+    fi
+fi
+
