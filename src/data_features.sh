496853a2683d8f142a009c467b40dc3a57644542
AleixMT <aleixaretra@gmail.com>
2021-12-12 17:25:12 +0100

    FIXED: Misspelled property packagedependencies

--- src/data_features.sh
+++ src/data_features.sh
@@ -6515,7 +6515,7 @@ tilix_readmeline="| Tilix | Advanced GTK3 tiling terminal emulator | Command \`t
 
 tmux_installationtype="packagemanager"
 tmux_arguments=("tmux")
-tmux_dependencies=("xdotool" "xclip" "tmuxp" "xsel" "bash-completion")
+tmux_packagedependencies=("xdotool" "xclip" "tmuxp" "xsel" "bash-completion")
 tmux_readmelinedescription="Terminal multiplexer for Unix-like operating systems"
 tmux_launchercontents=("
 [Desktop Entry]
