affb3d55db2261d3553f34347c19582e1c833efa
Axel <axfe999@vidalibarraquer.net>
2022-01-13 23:29:20 +0100

    UPDATED: Icons trimming script

--- src/tools/get_system_icons.sh
+++ src/tools/get_system_icons.sh
@@ -19,18 +19,24 @@ main()
   DIR="${CUSTOMIZER_PROJECT_FOLDER}/src/core"
   source "${CUSTOMIZER_PROJECT_FOLDER}/src/core/data_features.sh"
 
+  programs_with_copy_launcher=()
+  programs_trimmed=()
+  programs_not_trimmed=()
   for key_name in "${feature_keynames[@]}"; do
     num=1
     launchers_pointer="${key_name}_launchernames[@]"
     if [ -z "$(echo "${!launchers_pointer}")" ]; then
       continue
     fi
+    programs_with_copy_launcher+=(${key_name})
     mkdir -p "${CUSTOMIZER_PROJECT_FOLDER}/data/static/${key_name}"
     for launcher_pointer in ${!launchers_pointer}; do
       if [ ! -f "${XDG_DESKTOP_DIR}/${launcher_pointer}.desktop" ]; then
         echo "${XDG_DESKTOP_DIR}/${launcher_pointer}.desktop does not exist"
+        programs_not_trimmed+=(${key_name})
         continue
       fi
+      programs_trimmed+=(${key_name})
       icon_name="$(cat "${XDG_DESKTOP_DIR}/${launcher_pointer}.desktop" | grep -Eo "^Icon=.*\$" | cut -d "=" -f2)"
       for icon_path_system in $(find /usr/share/icons | grep "${icon_name}"); do
         echo "${icon_path_system}"
@@ -41,7 +47,24 @@ main()
     done
   done
 
+
+  echo
+  echo
+  echo "Programs with copy launcher:"
+  echo "${programs_with_copy_launcher[@]}"
+
+  echo
+  echo
+  echo "Programs trimmed:"
+  echo "${programs_trimmed[@]}"
+
+  echo
+  echo
+  echo "Programs not trimmed:"
+  echo "${programs_not_trimmed[@]}"
 }
+
+
 
 set -e
 main "$@"
