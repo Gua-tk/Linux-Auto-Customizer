c57b58751df6a16bf039db234ce22968ac82200f
Axel <axelfernandezcurros@gmail.com>
2022-04-11 21:48:02 +0200

    UPDATED: Refactor of warning

--- src/core/data_common.sh
+++ src/core/data_common.sh
@@ -232,16 +232,10 @@ if [ "${EUID}" != 0 ]; then
       echo "WARNING: ${HOME_FOLDER}/.config/user-dirs.dirs has not been found for the definition of XDG_DESKTOP_DIR,
       XDG_PICTURES_DIR, XDG_TEMPLATES_DIR in this context, falling back to defaults "
       echo -en "\e[0m"
-      declare XDG_DESKTOP_DIR="${HOME_FOLDER}/Desktop"
-      declare XDG_PICTURES_DIR="${HOME_FOLDER}/Pictures"
-      declare XDG_TEMPLATES_DIR="${HOME_FOLDER}/Templates"
+      declare -r XDG_DESKTOP_DIR="${HOME_FOLDER}/Desktop"
+      declare -r XDG_PICTURES_DIR="${HOME_FOLDER}/Pictures"
+      declare -r XDG_TEMPLATES_DIR="${HOME_FOLDER}/Templates"
     fi
-  fi
-
-  # Override these paths to the corresponding paths in the Windows system when in WSL2
-  if [ "${OS_NAME}" == "WSL2" ]; then
-    declare -r XDG_TEMPLATES_DIR="${HOME_FOLDER_WSL2}/Templates"
-    declare -r XDG_PICTURES_DIR="${HOME_FOLDER_WSL2}/Pictures"
   fi
 else
 
