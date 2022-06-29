d45b60e78d4c27c744b49ba32ee64a08325d0aaa
AleixMT <aleixaretra@gmail.com>
2022-06-29 15:30:53 +0200

    FIXED: Forced the pwd to be in customizer project folder when activating hooks in local mode

--- src/core/subsystems/activate_hooks.sh
+++ src/core/subsystems/activate_hooks.sh
@@ -1,5 +1,9 @@
 # Activate customizer hooks to enforce repository rules
 # Use hooks in the repo if git is installed
 if which git &>/dev/null; then
-  git config --local core.hooksPath "${CUSTOMIZER_PROJECT_FOLDER}/.githooks/"
+  # Subshell with no re-interpretation of output
+  (
+    cd "${CUSTOMIZER_PROJECT_FOLDER}" || exit 1
+    git config --local core.hooksPath "${CUSTOMIZER_PROJECT_FOLDER}/.githooks/"
+  )
 fi
