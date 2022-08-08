cff822c8c5b9d273cb74b93b8801b539f0ea7d5c
Axel <axelfernandezcurros@gmail.com>
2022-08-08 05:24:52 +0200

    UPDATED: Misspell in julia

--- data/core/feature_data.sh
+++ data/core/feature_data.sh
@@ -1672,7 +1672,7 @@ julia_arguments=("julia")
 julia_binariesinstalledpaths=("bin/julia;julia")
 julia_downloadKeys=("bundle")
 julia_bundle_URL="https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.5-linux-x86_64.tar.gz"
-julia_launcherkeynames="defaultLauncher"
+julia_launcherkeynames=("defaultLauncher")
 julia_defaultLauncher_terminal="true"
 
 jupyterLab_name="Jupyter Lab"
