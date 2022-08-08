c9211a26e50bad3f4f9bd7b44965363ee730f1ac
AleixMT <aleixaretra@gmail.com>
2022-08-08 18:27:49 +0200

    REMOVED: exec of default launcher of customizer GUI, deduced from context

--- src/core/data_features.sh
+++ src/core/data_features.sh
@@ -829,7 +829,6 @@ customizerGUI_multiuninstall_content="multiuninstall.json"
 customizerGUI_debug_path="${BIN_FOLDER}/customizerGUI/conf/runners/debug.json"
 customizerGUI_debug_content="debug.json"
 customizerGUI_launcherkeynames=("defaultLauncher") # TODO: pgadmin launcher style, with xdg-open to https://localhost:5000
-customizerGUI_defaultLauncher_exec="customizerGUI"
 
 install_customizerGUI_post()
 {
