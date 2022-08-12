f38b6ba2a0c4168c461db489f9cf358a3538d2af
Axel <axelfernandezcurros@gmail.com>
2022-08-12 19:29:35 +0200

    UPDATED: Adding TODO to notflix.sh

--- data/features/notflix/notflix.sh
+++ data/features/notflix/notflix.sh
@@ -43,6 +43,7 @@ notflix() {
   echo "LAUNCHING PEERFLIX"
 
   if [[ -f ${tmp_srt} ]]; then
+    # TODO: The code seems to ignore this condition when peerflix has downloaded the .srt file
     # When .srt file has been downloaded by peerflix
     peerflix -l "${magnet}" -t ${tmp_srt} --vlc --fullscreen
   else
