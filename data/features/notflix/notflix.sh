4abf89fff8ab2d6e06524bc507049421482b6f20
Axel <axelfernandezcurros@gmail.com>
2022-08-12 19:28:34 +0200

    UPDATED: Adding subtitles

--- data/features/notflix/notflix.sh
+++ data/features/notflix/notflix.sh
@@ -10,7 +10,8 @@ notflix_dependencies() {
     ln -s "€{BIN_FOLDER}/nodejs/bin/peerflix" "€{PATH_POINTED_FOLDER}/peerflix"
   fi
 }
-
+# TODO: BUG Add the year of the film to the search of the subtitle to avoid collisions with remakes,
+# TODO: in some cases it seems to download an .html page instead the .srt, in other cases it downloads a random .srt
 notflix() {
   if ! which peerflix; then
     notflix_dependencies
@@ -37,7 +38,35 @@ notflix() {
   fourteenthWordMovieName="$(echo $movie | cut -d "/" -f3 | cut -d "-" -f14)"
   fifteenthWordMovieName="$(echo $movie | cut -d "/" -f3 | cut -d "-" -f15)"
   sixteenthWordMovieName="$(echo $movie | cut -d "/" -f3 | cut -d "-" -f16)"
+
+  local -r tmp_srt="/tmp/torrent-stream/"*"/"*"/"*"${firstWordMovieName}"*"${secondWordMovieName}"*"${thirdWordMovieName}"*"${fourthWordMovieName}"*"${fifthWordMovieName}"*"${sixthWordMovieName}"*"${seventhWordMovieName}"*"${eigthWordMovieName}"*"${ninethWordMovieName}"*"${tenthWordMovieName}"*"${eleventhWordMovieName}"*"${twelvethWordMovieName}"*"${thirteenthWordMovieName}"*"${fourteenthWordMovieName}"*"${fifteenthWordMovieName}"*"${sixteenthWordMovieName}"*".srt"
   echo "LAUNCHING PEERFLIX"
-  peerflix -l "${magnet}" -t "/tmp/torrent-stream/"*"/"*"/"*"${firstWordMovieName}"*"${secondWordMovieName}"*"${thirdWordMovieName}"*"${fourthWordMovieName}"*"${fifthWordMovieName}"*"${sixthWordMovieName}"*"${seventhWordMovieName}"*"${eigthWordMovieName}"*"${ninethWordMovieName}"*"${tenthWordMovieName}"*"${eleventhWordMovieName}"*"${twelvethWordMovieName}"*"${thirteenthWordMovieName}"*"${fourteenthWordMovieName}"*"${fifteenthWordMovieName}"*"${sixteenthWordMovieName}"*".srt" --vlc --fullscreen
+
+  if [[ -f ${tmp_srt} ]]; then
+    # When .srt file has been downloaded by peerflix
+    peerflix -l "${magnet}" -t ${tmp_srt} --vlc --fullscreen
+  else
+    # Downloading .srt file from opensubtitles manually
+      echo ".srt file not downloaded via peerflix, performing manual downloaof .srt file from opensubtitles.org"
+      movieSubtitlesSearchPage=$(curl -s "https://www.opensubtitles.org/es/search2/sublanguageid-spa,spl/moviename-${query}" | grep -Eo '"(http|https)://www.opensubtitles.org/es/[a-zA-Z0-9#~.*,/!?=+&_%:-]*"' | head -1 | tr -d '"')
+      movieSubtitlesLink=$(curl -s "${movieSubtitlesSearchPage}" | grep -Eo "https?://www.opensubtitles.org/es/\S+?\"" | head -1 | tr -d '"')
+      movieLink=$(curl -s "${movieSubtitlesLink}" | grep -Eo 'href="https://www.opensubtitles.org/es/[a-zA-Z0-9#~.*,/!?=+&_%:-]*' | head -1 | cut -d '=' -f2 | tr -d '"')
+      movieURL="$(curl -s "${movieLink}" | grep -Eo '/es/search/sublanguageid-spa,spl/idmovie-[0-9]*' | head -1)"
+      movieURL="https://www.opensubtitles.org${movieURL}"
+      subtitleURL="$(curl -s "${movieURL}" | grep -Eo '[a-zA-Z0-9#~.*,/!?=+&_%:-]*-es' | head -1)"
+      subtitleURL="https://www.opensubtitles.org${subtitleURL}"
+      subtitleDownloadURL="$(curl -s "${subtitleURL}" | grep -Eo '/es/subtitleserve/sub/[0-9]*' | head -1)"
+      subtitleDownloadURL="https://www.opensubtitles.org${subtitleDownloadURL}"
+      subtitleNum="$(echo $subtitleDownloadURL | rev | cut -d '/' -f1 | rev)"
+      # Download the .srt file to ~/Documents/subtitles
+      mkdir -p ~/Documents/subtitles
+      wget -P ~/Documents/subtitles "${subtitleDownloadURL}"
+      unzip ~/Documents/subtitles/${subtitleNum} -d ~/Documents/subtitles
+      rm ~/Documents/subtitles/${subtitleNum}
+      rm ~/Documents/subtitles/*.nfo
+      local -r subtitlePath="$HOME/Documents/subtitles/"*".srt"
+      peerflix -l "${magnet}" -t ${subtitlePath} --vlc --fullscreen
+  fi
   echo "EXIT PEERFLIX"
+  rm -rf ~/Documents/subtitles
 }
