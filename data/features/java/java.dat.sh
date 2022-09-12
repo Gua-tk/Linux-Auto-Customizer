9591f0742a8863579035a78ed30d18d2f97719b3
AleixMT <aleixaretra@gmail.com>
2022-09-12 13:14:37 +0200

    UPDATED: java dynamic download to the last version

---
+++ data/features/java/java.dat.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+java_name="Java Development Kit 8"
+java_description="Implementation of version 8 of the Java (programming language) SE Platform + the tools for a developer"
+java_version="Oracle JDK 8.LatestRelease - Linux x64"
+java_tags=("programming" "development")
+java_systemcategories=("Development" "Languages")
+java_bashfunctions=("java.sh")
+java_binariesinstalledpaths=("bin/java;java" "bin/keytool;keytool")
+java_downloadKeys=("bundle")
+java_bundle_URL="https://download.oracle.com/java/18/latest/jdk-18_linux-x64_bin.tar.gz"
