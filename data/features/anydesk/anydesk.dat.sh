a89bcc96a882d938402d2eaa87ddd35ba7a422c9
AleixMT <aleixaretra@gmail.com>
2022-12-02 16:19:32 +0100

    UPDATED: anydesk version

--- data/features/anydesk/anydesk.dat.sh
+++ data/features/anydesk/anydesk.dat.sh
@@ -8,5 +8,5 @@ anydesk_packagedependencies=("libminizip1" "libgtkglext1")
 anydesk_bashfunctions=("silentFunction")
 anydesk_binariesinstalledpaths=("anydesk;anydesk")
 anydesk_downloadKeys=("bundle")
-anydesk_bundle_URL="https://download.anydesk.com/linux/anydesk-6.1.1-amd64.tar.gz"
+anydesk_bundle_URL="https://download.anydesk.com/linux/anydesk-6.2.1-amd64.tar.gz"
 anydesk_launcherkeynames=("default")
