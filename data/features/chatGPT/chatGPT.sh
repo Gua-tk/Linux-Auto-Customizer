53f0e6f0a327d0c985c5eb07f41ba843672b4dfe
axlfc <axelfernandezcurros@gmail.com>
2022-12-12 13:08:04 +0100

    UPDATED: Adding codeGPT function

--- data/features/chatGPT/chatGPT.sh
+++ data/features/chatGPT/chatGPT.sh
@@ -24,3 +24,15 @@ talkGPT()
     "€{BIN_FOLDER}/chatGPT/bin/python3" "€{BIN_FOLDER}/chatGPT/Content/Python/chatBot/talk.py"
   fi
 }
+
+codeGPT()
+{
+  if [ ! -f "€{BIN_FOLDER}/chatGPT/Content/Python/chatBot/.env" ]; then
+    echo "You need to enter a valid API key in the file €{BIN_FOLDER}/chatGPT/Content/Python/chatBot/.env"
+    echo "Go to https://beta.openai.com/account/api-keys to get yours."
+    echo "Edit or create the .env file and write inside the file:"
+    echo "OPENAI_API_KEY=your_open_ai_api_key_code"
+  else
+    "€{BIN_FOLDER}/chatGPT/bin/python3" "€{BIN_FOLDER}/chatGPT/Content/Python/chatBot/code.py" "$1"
+  fi
+}
