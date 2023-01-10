495ae937316ccc07cd61c7c1ebfbd94d2658c0cf
AleixMT <aleixaretra@gmail.com>
2023-01-10 11:39:35 +0100

    UPDATED: node js, npm and npx upgraded to v18

--- data/features/nodejs/nodejs.dat.sh
+++ data/features/nodejs/nodejs.dat.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 nodejs_name="NodeJS"
 nodejs_description="JavaScript language for the developers."
-nodejs_version=""
+nodejs_version="v18.13.0-linux-x64"
 nodejs_tags=("javascript" "language" "packageManager" "npm")
 nodejs_systemcategories=("Languages" "Development")
 nodejs_downloadKeys=("bundle")
-nodejs_bundle_URL="https://nodejs.org/dist/v14.17.0/node-v14.17.0-linux-x64.tar.xz"
+nodejs_bundle_URL="https://nodejs.org/dist/v18.13.0/node-v18.13.0-linux-x64.tar.xz"
 nodejs_binariesinstalledpaths=("bin/node;node" "bin/npm;npm" "bin/npx;npx")
 nodejs_launcherkeynames=("languageLauncher")
 nodejs_languageLauncher_terminal="true"
