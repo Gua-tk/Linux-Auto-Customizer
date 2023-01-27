6957832c0da78cfb6a566436f9c3fcb48b3ef3e1
AleixMT <aleixaretra@gmail.com>
2023-01-27 13:04:00 +0100

    UPDATED: Minor changes
    Co-authored-by: Axlfc <axelfernandezcurros@gmail.com>

--- data/features/nodejs/nodejs.func.sh
+++ data/features/nodejs/nodejs.func.sh
@@ -8,6 +8,7 @@ install_nodejs_post()
     output_proxy_executioner "Can't update npm because you are root, re-run 'npm install npm@latest -g' if you want to update npm" "WARNING"
   fi
 }
+
 uninstall_nodejs_post()
 {
   :
