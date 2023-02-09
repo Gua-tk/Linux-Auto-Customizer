726f47b415741d336309433286824b89e25fbf5e
AleixMT <aleixaretra@gmail.com>
2023-02-09 17:57:10 +0100

    CHANGED: -e option of the gnome-terminal command substituted, also note that the terminal option has been deleted from all the affected features, since it is not really necessary to set terminal to true when starting a program from a  launcher, wvwn when it runs inside a terminal

--- data/features/julia/julia.dat.sh
+++ data/features/julia/julia.dat.sh
@@ -9,5 +9,4 @@ julia_binariesinstalledpaths=("bin/julia;julia")
 julia_downloadKeys=("bundle")
 julia_bundle_URL="https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.5-linux-x86_64.tar.gz"
 julia_launcherkeynames=("defaultLauncher")
-julia_defaultLauncher_terminal="true"
-julia_defaultLauncher_exec="gnome-terminal -e julia"
+julia_defaultLauncher_exec="gnome-terminal -- julia"
--- data/features/scala/scala.dat.sh
+++ data/features/scala/scala.dat.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-
 scala_name="Scala"
 scala_description="Programming language"
 scala_version="System dependent"
 scala_tags=("scala")
 scala_systemcategories=("Languages")
+
 scala_packagenames=("scala")
 scala_launcherkeynames=("languageLauncher")
-scala_languageLauncher_terminal="true"
-scala_languageLauncher_exec="gnome-terminal -e scala"
+scala_languageLauncher_exec="gnome-terminal -- scala"
