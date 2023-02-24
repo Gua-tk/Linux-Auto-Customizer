09906d1eda503d6794c4964df4d56cbfdf9b6305
AleixMT <aleix.marine@estudiants.urv.cat>
2023-02-24 15:33:22 +0100

    FINISHED: tool to apply tags

--- src/tools/apply_tags.sh
+++ src/tools/apply_tags.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 ########################################################################################################################
-# -Name: apply_tags.sh
-# -Description: Inserts in the last position of the field tags o fhte supplied features
-# -Creation Date: 02/02/2022
-# -Last Modified: 02/02/2002
-# -Author: Aleix Mariné-Tena
-# -Email: aleixaretra@gmail.com
-# -Permissions: It is run with normal permissions.
-# -Args: No arguments
-# -Usage: Go to Linux-Auto-Customizer/src/features and run 'bash ../tools/add_functions_shebang.sh'
-# -License: GPL v2.0
+# - Name: apply_tags.sh
+# - Description: Inserts in the last position of the tags field of the supplied feature the supplied tag name to apply.
+# - Creation Date: 24/02/2023
+# - Last Modified: 24/02/2023
+# - Author: Aleix Mariné-Tena
+# - Email: aleixaretra@gmail.com
+# - Permissions: It is run with normal permissions. Modification access to the .dat.sh files of the selected feature.
+# - Arguments:
+#   * Argument 1: Name of the tag to apply.
+#   * Argument 2, 3, 4... : Feature key names to apply the tag selection.
+# - Usage: Go to Linux-Auto-Customizer/src/tools and run 'bash apply_tags.sh'
+# - License: GPL v2.0
 ########################################################################################################################
 
 main()
@@ -27,14 +29,14 @@ main()
   shift
 
   for featureName in "$@"; do
-    printf '\n\nApplying tag to feature %s' "${featureName}"
+    printf '\nApplying tag to feature %s\n' "${featureName}"
 
     if [ ! -f "${CUSTOMIZER_PROJECT_FOLDER}/data/features/${featureName}/${featureName}.dat.sh" ]; then
       echo "ERROR ${CUSTOMIZER_PROJECT_FOLDER}/data/features/${featureName}/${featureName}.dat.sh file not detected, skipping ${featureName}"
     fi
     local tagsContent="$(grep -Eo "${featureName}_tags=(.*)" < "${CUSTOMIZER_PROJECT_FOLDER}/data/features/${featureName}/${featureName}.dat.sh")"
     local tagsSplit="$(echo "${tagsContent}" | cut -d ")" -f1)"
-    local newTagContent="$(echo "${tagsSplit} \"${tagName}\")")"
+    local newTagContent="${tagsSplit} \"${tagName}\")"
 
     sed -i "s/${tagsContent}/${newTagContent}/g" "${CUSTOMIZER_PROJECT_FOLDER}/data/features/${featureName}/${featureName}.dat.sh"
   done
