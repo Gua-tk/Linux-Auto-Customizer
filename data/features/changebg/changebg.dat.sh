2194e128e3be191dbd5099651be13e12abb6d217
Axlfc <axelfernandezcurros@gmail.com>
2023-03-10 16:33:10 +0100

    UPDATED: changebg cronjob data

--- data/features/changebg/changebg.dat.sh
+++ data/features/changebg/changebg.dat.sh
@@ -8,20 +8,6 @@ changebg_movefiles=("*.jpg;${XDG_PICTURES_DIR}/wallpapers" "*.png;${XDG_PICTURES
 changebg_binariesinstalledpaths=("cronscript.sh;changebg")
 changebg_cronscript_content="cronscript.sh"
 changebg_cronscript_path="cronscript.sh"
-changebg_cronjob_content="cronjob"
-changebg_cronjob_path="cronjob"
-changebg_filekeys=("cronscript" "cronjob")
-changebg_manualcontentavailable="0;0;1"
+changebg_filekeys=("cronscript")
 changebg_repositoryurl="https://github.com/AleixMT/wallpapers"
-install_changebg_post() {
-  # TODO: Do not append to crontab if an exact job already exists
-  if [ -n "${SUDO_USER}" ]; then
-    (crontab -u "${SUDO_USER}" -l ; cat "${BIN_FOLDER}/changebg/cronjob") | crontab -u "${SUDO_USER}" -
-  else
-    (crontab -l ; cat "${BIN_FOLDER}/changebg/cronjob") | crontab -
-  fi
-}
-uninstall_changebg_post() {
-  :
-  #crontab "${USR_BIN_FOLDER}/changebg/cronjob"
-}
+changebg_cronjobs=("cronjob")
