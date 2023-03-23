b421c272e8d4ab9c78d7e2f975d81521e7ea2645
axlfc <axelfernandezcurros@gmail.com>
2023-03-23 10:37:31 +0100

    UPDATED: removing unnecessary dependency

--- data/features/ssh/ssh.dat.sh
+++ data/features/ssh/ssh.dat.sh
@@ -7,7 +7,7 @@ ssh_tags=("customDesktop")
 ssh_systemcategories=("ConsoleOnly" "Network" "RemoteAccess" "Shell")
 
 ssh_packagenames=("ssh-client")
-ssh_packagedependencies=("openssh-sftp-server" "openssh-client")
+ssh_packagedependencies=("openssh-client")
 ssh_launcherkeynames=("defaultLauncher")
 ssh_defaultLauncher_exec="nemo-connect-server"
 ssh_defaultLauncher_name="Connect to a Server"
