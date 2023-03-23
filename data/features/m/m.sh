a70188c30e73a62f392899772c4d35bbfe064258
axlfc <axelfernandezcurros@gmail.com>
2023-03-23 14:09:07 +0100

    UPDATED: solving conversations suggested for function m. Not handling multi-line argument definitions.

--- data/features/m/m.sh
+++ data/features/m/m.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/env bash
 
+# Description:
+# Argument 1: String representing a system binary (example: ls)
+# Argument 2 (optional): String representing argument option of the binary
 m() {
-  if command -v man >/dev/null; then
-    if [[ $# -eq 1 ]]; then
-      man_output=$(man "$@" | grep -E '^ +-[a-zA-Z], --[a-zA-Z-]+')
-      echo "$man_output"
-    elif [[ $# -eq 2 ]]; then
-      man "$1" | grep -E "^ +$2\b" -A1 | sed 's/^\s\+//'
-    else
-      echo "Error: Invalid arguments." >&2
-      return 1
-    fi
-  else
-    echo "Error: 'man' command not found." >&2
+  if ! command -v man &>/dev/null; then
+    echo "Error: 'man' command not found."
     return 1
+  fi
+
+  if [ $# -lt 1 ]; then
+    echo "Error: Invalid arguments."
+    return 1
+  fi
+
+  if [[ $# -eq 1 ]]; then
+    # Matches any line that starts with one or more spaces followed by a dash (-), one letter (lowercase or uppercase),
+    # a comma, a space, two hyphens (--), and one or more letters (lowercase or uppercase) or dashes (-).
+    # This pattern is used to match the short and long options that are documented in the manual page.
+    man_output="$(man "$@" | grep -E '^ +-[a-zA-Z], --[a-zA-Z-]+')"
+    echo "${man_output}"
+  else
+    for arg in "${@:2}"; do
+      # search for lines that start with the second argument ($2) preceded by one or more spaces (^ +) and followed by
+      # a word boundary (\b). The -A1 option tells grep to also output one line of context after each match.
+      # Remove the leading whitespace from the lines that were matched in the previous step.
+      man "${1}" | grep -E "^ +${arg}\\b" -A3 | sed 's/^\s\+//'
+    done
   fi
 }
