0982b256069bc9cce368187a0f83bed93b1a4903
Axlfc <axel.fernandez@ludusglobal.com>
2023-04-17 09:28:28 +0200

    UPDATED: Changing descriptions, updating guard clauses

--- data/features/m/m.sh
+++ data/features/m/m.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Description: Function to search arguments in the manual of a command, with more than two arguments looks for all
-# arguments from the first to the command manual for their definition
+# Description: Function to search text in the manual of a command. It expects at least one argument, which should
+# contain the name of a system command. The rest of the arguments are the therms searched in the manual.
 # Argument 1: String representing a system binary (example: ls)
 # Argument 2 (optional): String representing argument option of the binary (example: -A)
 m() {
@@ -17,11 +17,12 @@ m() {
 
   if [ $# -eq 1 ]; then
     man "$1"
-  else
-    for arg in "${@:2}"; do
-      # search for lines that start with the second argument ($2) preceded by one or more spaces (^ +)
-      # The -A5 option tells grep to also output five lines of context after each match.
-      man "$1" | grep -hnI -E "+${arg}" -B 5 -A 5 --color='auto'
-    done
+    return 0
   fi
+
+  for arg in "${@:2}"; do
+    # Search the manual page for the command specified by the first argument ($1)
+    # using the 'man' command, and pipe the output to 'grep' for searching the arguments of the command.
+    man "$1" | grep -hnI -E "+${arg}" -B 5 -A 5 --color='auto'
+  done
 }
