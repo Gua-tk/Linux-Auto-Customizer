7072bbbd59feedc00887c2ebbb87a5fb2977fa1c
Axlfc <axel.fernandez@ludusglobal.com>
2023-04-28 12:24:13 +0200

    UPDATED: m.dat.sh

--- data/features/m/m.dat.sh
+++ data/features/m/m.dat.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-m_name="Function a"
+m_name="Function m"
 m_description="Function to search in the manual"
 m_version="1.0"
-m_tags=("info" "search" "terminal" "customDesktop" "bashFunctions")
+m_tags=("search" "customDesktop" "bashFunctions")
 m_systemcategories=("System" "Utility")
 m_bashfunctions=("m.sh")
