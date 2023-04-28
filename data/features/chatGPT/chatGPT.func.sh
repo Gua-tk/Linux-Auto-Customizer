f0329efcf737458a085eb9a09672a2fe512f0d9c
Axlfc <axel.fernandez@ludusglobal.com>
2023-04-28 13:34:25 +0200

    UPDATED: chatGPT.func.sh uninstall condition to remove symbolic link to conversations folder.

--- data/features/chatGPT/chatGPT.func.sh
+++ data/features/chatGPT/chatGPT.func.sh
@@ -2,9 +2,10 @@ install_chatGPT_mid() {
   if [ ! -f "${BIN_FOLDER}/chatGPT/conversations" ]; then
     mkdir "${BIN_FOLDER}/chatGPT/conversations"
   fi
+  rm "${XDG_DOCUMENTS_DIR}/chatGPT_conversations"
   ln -s "${BIN_FOLDER}/chatGPT/conversations" "${XDG_DOCUMENTS_DIR}/chatGPT_conversations"
 }
 
 uninstall_chatGPT_mid() {
-  :
+  rm "${XDG_DOCUMENTS_DIR}/chatGPT_conversations"
 }
