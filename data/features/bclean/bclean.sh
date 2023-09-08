2f7ab3e796016ec7f4a8267ae2fbbd0039018802
AleixMT <aleixaretra@gmail.com>
2023-09-08 16:10:43 +0200

    UPDATED: unified behaviour

--- data/features/bclean/bclean.sh
+++ data/features/bclean/bclean.sh
@@ -3,16 +3,33 @@
 bclean()
 {
   if [ "$1" == "--interactive" ] || [ "$1" == "-i" ]; then
+    # Delete gone
+    git fetch -p && \
+    for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do
+       echo "${branch}" >> /tmp/merged-branches
+    done
+
+    # Delete without remote
+    IFS=$'\n'
+    git fetch -p && \
+    for line in $(git for-each-ref --format '%(refname) %(upstream)' refs/heads); do
+      # If the second field is empty, it has no upstream
+      if [ -z "$(echo "${line}" | cut -d " " -f2)" ]; then
+        # Detect name and delete it
+        echo "${line}" | cut -d " " -f1 | rev | cut -d "/" -f1 | rev >> /tmp/merged-branches
+      fi
+    done
+
+    # delete merged
     current="$(git branch --show-current)"
     git branch --merged \
     | sed '/^\** *develop$/d' \
     | sed '/^\** *master$/d' \
-    | sed "/^\** *${current}/d" >/tmp/merged-branches \
-    && grep -q '[^[:space:]]' < "/tmp/merged-branches" \
-    && nano /tmp/merged-branches \
+    | sed "/^\** *${current}/d" >>/tmp/merged-branches \
     && grep -q '[^[:space:]]' < "/tmp/merged-branches" \
     && xargs git branch -d </tmp/merged-branches
     unset current
+
   else
     # Delete gone
     git fetch -p && \
