9e53bcd7459da57d24d48469d3db1c84b8e1e67f
AleixMT <aleixaretra@gmail.com>
2023-09-08 13:07:32 +0200

    UPDATED: screenshots information and binary shortcuts

--- data/features/screenshots/screenshots.dat.sh
+++ data/features/screenshots/screenshots.dat.sh
@@ -5,13 +5,14 @@ screenshots_description="Takes a screenshot and saves it to custom ~/Images/scre
 screenshots_version="System dependent"
 screenshots_tags=("gnome" "customDesktop")
 screenshots_systemcategories=("Utility")
+
 screenshots_filekeys=("screenshotwindow" "screenshotarea" "screenshotfull")
-screenshots_keybindings=("scr-area;<Primary><Alt><Super>a;Screenshot Area" "scr-full;<Primary><Alt><Super>s;Screenshot Full" "scr-window;<Primary><Alt><Super>w;Screenshot Window")
+screenshots_keybindings=("screenshot-area;<Primary><Alt><Super>a;Screenshot Area" "screenshot-full;<Primary><Alt><Super>s;Screenshot Full" "screenshot-window;<Primary><Alt><Super>w;Screenshot Window")
 screenshots_screenshotwindow_path="screenshot_window.sh"
 screenshots_screenshotwindow_content="screenshot_window.sh"
 screenshots_screenshotarea_path="screenshot_area.sh"
 screenshots_screenshotarea_content="screenshot_area.sh"
 screenshots_screenshotfull_path="screenshot_full.sh"
 screenshots_screenshotfull_content="screenshot_full.sh"
-screenshots_binariesinstalledpaths=("screenshot_area.sh;scr-area" "screenshot_window.sh;scr-window" "screenshot_full.sh;scr-full")
+screenshots_binariesinstalledpaths=("screenshot_area.sh;screenshot-area" "screenshot_window.sh;screenshot-window" "screenshot_full.sh;screenshot-full")
 screenshots_packagedependencies=("gnome-screenshot" "xclip")
---
+++ data/features/screenshots/screenshots.md
@@ -0,0 +1,9 @@
+### Description
+This feature adds new shortcuts to perform screenshots. It also adds new binaries in path to call the screenshot 
+functionality. There are three types of screenshots: area, window, screen.
+
+|                   | area                   | window                 | screen                 | 
+|-------------------|------------------------|------------------------|------------------------|
+| path link         | screenshot-area        | screenshot-window      | screenshot-screen      |   
+| keyboard shortcut | <Primary><Alt><Super>a | <Primary><Alt><Super>w | <Primary><Alt><Super>s |   
+
