eb57af8fb21c1c058ce66152dc4fd269f9ecaa4e
AleixMT <aleixaretra@gmail.com>
2023-09-08 15:07:11 +0200

    UPDATED: changed format to many lines

--- data/features/bclean/bclean.sh
+++ data/features/bclean/bclean.sh
@@ -2,10 +2,21 @@
 
 bclean()
 {
-  if [ "$1" == "--interactive" ]; then
-    git branch --merged >/tmp/merged-branches && \ vi /tmp/merged-branches && xargs git branch -d </tmp/merged-branches
+  if [ "$1" == "--interactive" ] || [ "$1" == "-i" ]; then
+    current="$(git branch --show-current)"
+    git branch --merged \
+    | sed '/^\** *develop$/d' \
+    | sed '/^\** *master$/d' \
+    | sed "/^\** *${current}/d" >/tmp/merged-branches \
+    && nano /tmp/merged-branches \
+    && grep -q '[^[:space:]]' < "/tmp/merged-branches" \
+    && xargs git branch -d </tmp/merged-branchesS
+    unset current
   else
-    git fetch -p && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D "${branch}"; done
+    git fetch -p \
+    && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do
+      git branch -D "${branch}"
+    done
   fi
 }
 
