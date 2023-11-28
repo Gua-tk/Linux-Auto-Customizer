119bbaf7e0c0846cd17351ea893fd1d47a0d0124
Axlfc <axlffcc@gmail.com>
2023-11-29 00:41:46 +0100

    UPDATED: Debugging F Function to ignore paths

--- data/features/FFunction/F.sh
+++ data/features/FFunction/F.sh
@@ -4,6 +4,17 @@ F() {
   if [ $# -eq 0 ]; then  # No arguments given
     find / 2>/dev/null
   else
+    if [[ "$1" =~ -i=.+ ]]; then
+    echo "$1olssssssssssssssssssssssssssssssssss"
+      ignored_paths="$(echo "${1}" | cut -d "=" -f2 | tr ',' ' ')"
+      find_args_ignored_paths=""
+      IFS=' '
+      for path in ${ignored_paths}; do
+        echo "$path entra en el for"
+        find_args_ignored_paths+=" -not -path '${path}' "
+      done
+      shift
+    fi
     if [ -d "$1" ]; then
       first_argument="$1"
       shift
@@ -12,7 +23,8 @@ F() {
     fi
     IFS=$'\n'
     while [ -n "$1" ]; do
-      for filename in $(find "${first_argument}" -type f -not -path '*/\.git/*' 2>/dev/null); do
+      echo "FIND IGNORED PATHS: ${find_args_ignored_paths}FidnBBBBBBBBBBBBBBBBBBBBBBB"
+      for filename in $(find "${first_argument}" -type f -not -path '*/\.git/*' ${find_args_ignored_paths} 2>/dev/null); do
         local result="$(grep "$1" < "${filename}" 2>/dev/null)"
         if [ -n "$(echo "${result}")" ]; then
           echo
