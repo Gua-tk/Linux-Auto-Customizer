74c09e6595c6a8ff98fc157b769a4c532df99633
AleixMT <aleixaretra@gmail.com>
2023-11-28 17:40:52 +0100

    UPDATED: a function conflict

--- data/features/a/a.dat.sh
+++ data/features/a/a.dat.sh
@@ -2,6 +2,6 @@
 a_name="Function a"
 a_description="A terminal shortcut"
 a_version="1.0"
-a_tags=("info" "search" "terminal" "customDesktop" "bashFunctions")
+a_tags=("info" "search" "customDesktop" "bashFunctions")
 a_systemcategories=("System" "Utility")
 a_bashfunctions=("a.sh")
