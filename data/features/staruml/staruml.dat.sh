56eebc008c9f582e29fa581462efea6c8c7d103a
AleixMT <aleixaretra@gmail.com>
2024-01-03 14:48:44 +0100

    ADDED: star UML installation

---
+++ data/features/staruml/staruml.dat.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+staruml_name="Star UML"
+staruml_description="File Synchronizer"
+staruml_version="2020.03.04_amd64"
+staruml_tags=("uml")
+staruml_systemcategories=("Development" "Network")
+
+staruml_downloadKeys=("bundle")
+staruml_bundle_URL="https://staruml.io/api/download/releases-v6/StarUML_6.0.1_amd64.deb"
+staruml_package_manager_override="apt-get"
+staruml_launcherkeynames=("defaultLauncher")
+staruml_defaultLauncher_exec="staruml"
+staruml_defaultLauncher_notify="false"
+staruml_bashfunctions=("silentFunctionInWd")
