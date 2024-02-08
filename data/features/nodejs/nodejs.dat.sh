23743f95a0f92458064aaffb884644ff9527452e
AleixMT <aleixaretra@gmail.com>
2024-02-09 00:04:53 +0100

    UPDATED: nodejs version

--- data/features/nodejs/nodejs.dat.sh
+++ data/features/nodejs/nodejs.dat.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 nodejs_name="NodeJS"
 nodejs_description="JavaScript language for the developers."
-nodejs_version="v16.19.0-linux-x64"
+nodejs_version="v20.11.0-linux-x64"
 nodejs_tags=("language" "customDesktop")
 nodejs_systemcategories=("Development" "Languages")
 nodejs_downloadKeys=("bundle")
-nodejs_bundle_URL="https://nodejs.org/download/release/v16.19.0/node-v16.19.0-linux-x64.tar.gz"
+nodejs_bundle_URL="https://nodejs.org/dist/v20.11.0/node-v20.11.0-linux-x64.tar.xz"
 nodejs_binariesinstalledpaths=("bin/node;node" "bin/npm;npm" "bin/npx;npx")
 nodejs_launcherkeynames=("languageLauncher")
 nodejs_languageLauncher_exec="gnome-terminal -- nodejs"
