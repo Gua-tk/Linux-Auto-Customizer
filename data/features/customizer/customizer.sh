036eac401dfcf4b2b16d62ea2412382c291de656
AleixMT <aleixaretra@gmail.com>
2024-02-13 16:03:04 +0100

    ADDED: greeedy autocompletion

--- data/features/customizer/customizer.sh
+++ data/features/customizer/customizer.sh
@@ -2,14 +2,21 @@
 
 _customizer-install() {
   COMPREPLY=()
-  local arguments="$(echo "$(customizer-install --commands)")"
+  local arguments="$(echo "$(customizer --commands)")"
   COMPREPLY=( $(compgen -W "${arguments}" -- "${COMP_WORDS[COMP_CWORD]}") )
 }
 complete -F _customizer-install customizer-install
 
 _customizer-uninstall() {
   COMPREPLY=()
-  local arguments="$(echo "$(customizer-uninstall --commands)")"
+  local arguments="$(echo "$(customizer --commands)")"
   COMPREPLY=( $(compgen -W "${arguments}" -- "${COMP_WORDS[COMP_CWORD]}") )
 }
 complete -F _customizer-uninstall customizer-uninstall
+
+_customizer() {
+  COMPREPLY=()
+  local arguments="$(echo "$(customizer --commands)")"
+  COMPREPLY=( $(compgen -W "${arguments}" -- "${COMP_WORDS[COMP_CWORD]}") )
+}
+complete -F _customizer customizer
