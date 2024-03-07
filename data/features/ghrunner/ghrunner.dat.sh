b017c80a322ddd25264f2785696216b14c4cd41d
AleixMT <aleixaretra@gmail.com>
2024-03-07 17:43:50 +0100

    UPDATED: ghrunner config

--- data/features/ghrunner/ghrunner.dat.sh
+++ data/features/ghrunner/ghrunner.dat.sh
@@ -7,6 +7,8 @@ ghrunner_tags=("customServer")
 ghrunner_systemcategories=("Office" "Qt" "TextEditor" "WordProcessor")
 
 ghrunner_downloadKeys=("bundle")
+ghrunner_bundle_doNotInherit="yes"
+ghrunner_bundle_downloadPath="${BIN_FOLDER}/ghrunner/"
 ghrunner_bundle_URL="https://github.com/actions/runner/releases/download/v2.314.1/actions-runner-linux-x64-2.314.1.tar.gz"
 ghrunner_binariesinstalledpaths=("config.sh;gh-config" "run.sh;gh-run")
 ghrunner_bashfunctions=("ghrunner.sh")
