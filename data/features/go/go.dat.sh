543e35bb6c3324e7de907419168a2c2c9763f643
AleixMT <aleixaretra@gmail.com>
2024-03-07 16:21:12 +0100

    CHANGED: installation of go

--- data/features/go/go.dat.sh
+++ data/features/go/go.dat.sh
@@ -6,7 +6,7 @@ go_version="1.17.linux-amd64"
 go_systemcategories=("Languages")
 go_tags=("search" "customDesktop")
 
-go_downloadkeys=("bundle")
+go_downloadKeys=("bundle")
 go_bundle_URL="https://go.dev/dl/go1.22.1.linux-amd64.tar.gz"
 go_bashinitializations=("go.sh")
-pycharm_binariesinstalledpaths=("bin/go;go" "bin/gofmt;gofmt")
+go_binariesinstalledpaths=("bin/go;go" "bin/gofmt;gofmt")
